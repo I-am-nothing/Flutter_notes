887e60e956c415f8cf96c406593881ebd7706622
sakun921118 <jdps99119@gmail.com>
2022-05-10 01:30:48 +0800

    Step-11

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentDriectoryException implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
+class CouldNotDeleteUserExcpetion implements Exception {}
+
+class UserAlreadyExistsException implements Exception {}
+
+class CouldNotFindUserExcpetion implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
+
+class CouldNotFindNoteException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
